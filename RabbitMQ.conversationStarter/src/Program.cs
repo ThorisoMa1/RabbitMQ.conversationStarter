0d4996b85a8df1d54521dcd7205048e756ecc461
ThorisoMa1 <ThorisoMa1@gmail.com>
2023-06-04 15:04:14 +0200

    Added Folder structure Added initial program rabbit sender code moved programm cs to src folder

---
+++ RabbitMQ.conversationStarter/src/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory conFactory = new ConnectionFactory();
+
+conFactory.Uri = new Uri("amqp://guest:guest@localhost:15672");
+conFactory.ClientProvidedName = "RabbitMQ Conversation Starter";
+
+
+IConnection conn = conFactory.CreateConnection();
+IModel channel = conn.CreateModel();
+
+string exchangeName="demo-exchange";
+
+string routingKey = "demo-routing-key";
+
+string queueName = "DemoQueue";
+
+
+
+channel.ExchangeDeclare(exchangeName,ExchangeType.Direct);
+channel.QueueDeclare(queueName,false,false,false,null);
+channel.QueueBind(queueName,exchangeName,routingKey,null);
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hi There , lets start a conversation");
+
+channel.BasicPublish(exchangeName,routingKey,null,messageBodyBytes);
+
+channel.Close();
+conn.Close();
+
+
+
+
+
