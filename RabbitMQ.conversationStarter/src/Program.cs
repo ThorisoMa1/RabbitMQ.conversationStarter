18d80b90d6041410aee50f71e126863d04f78d94
ThorisoMa1 <ThorisoMa1@gmail.com>
2023-06-04 17:16:17 +0200

    Adjusted to the assignments required message for the sender

--- RabbitMQ.conversationStarter/src/Program.cs
+++ RabbitMQ.conversationStarter/src/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.DependencyInjection;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Logging;
 using RabbitMQ.conversationStarter;
 using RabbitMQ.conversationStarter.Infustructure;
 using RabbitMQ.conversationStarter.Interfaces;
@@ -12,12 +13,24 @@ using System.Text;
 // Configure the service provider
 var serviceProvider = startup.ConfigureServices();
 
-
 // Resolve and use your dependencies
 var messageSendingService = serviceProvider.GetService<IMessageSenderService>();
 
 
-messageSendingService.SendMessage("Hello, RabbitMQ!");
+
+Console.WriteLine("please enter your name and surname for the greeting and press enter ");
+string name = Console.ReadLine();
+
+string message = string.Format("Hello My name is {0}", name);
+
+Console.WriteLine("thank you , sending your message.......");
+
+// send the message 
+messageSendingService.SendMessage(message);
+
+Console.WriteLine("SENT");
+
+
 
 
 
